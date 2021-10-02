0751540c1975be04fa5cee43e1f699ec779c0b9a
azizaalhouni <azizaalhouni@yahoo.com>
2021-10-02 12:44:08 -0500

    update

--- TableLayout/app/src/main/java/com/example/tablelayout/MainActivity.kt
+++ TableLayout/app/src/main/java/com/example/tablelayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tablelayout
 
+import android.graphics.Color.red
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,7 @@ import android.widget.EditText
 import android.widget.TableRow
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.graphics.Color.RED as ColorRED
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,22 +26,29 @@ class MainActivity : AppCompatActivity() {
 
 //        val m = findViewById<EditText>(R.id.codeName)
         var columnOne = TextView(this)
-        var ColumnTwo = TextView(this)
+        var columnTwo = TextView(this)
+        columnOne.setBackgroundColor(0xfff00000.toInt())
+        columnTwo.setBackgroundColor(0xfff00000.toInt())
+       columnOne.setGravity(android.view.Gravity.CENTER)
+        columnTwo.setGravity(android.view.Gravity.CENTER)
 
-        (android.view.Gravity.CENTER)
         val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
         tableRow.setLayoutParams(layoutParams)
 
         tableRow.addView(columnOne,0)
-        tableRow.addView(ColumnTwo,1)
+        tableRow.addView(columnTwo,1)
+//        tableRow.background = "#E91E63"
 
 //       tableRow.background = R.drawable.ic_launcher_background
 
         //To add the created Row into Layout
         tableLay.addView(tableRow)
         columnOne.text = version.text.toString()
-        ColumnTwo.text = codeName.text.toString()
+        columnTwo.text = codeName.text.toString()
+
 
 
     }
 }
+
+
